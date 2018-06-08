cf46cd3b22a39ee2d7b415b61a59db7e1a7f9e21
zhw2590582 <laozhaochaguan@gmail.com>
2018-06-08 12:56:06 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
 (function ($) {
     $(function () {
-        const brusher = new Brusher({
-            image: './img/avatar.png',
-            keepCleared: true,
-            stroke: 20,
-            lineStyle: 'square',
-            autoBlur: false,
-            autoBlurValue: 15,
-        });
-        // brusher.init();
+        //
     });
 })(jQuery);
